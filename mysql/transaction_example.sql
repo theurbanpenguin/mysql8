162b3eb8353ac1c653ab5800621c0ec286bb30b5
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2025-01-06 17:44:46 +0000

    add transaction script

--- mysql/transaction_example.sql
+++ mysql/transaction_example.sql
@@ -1,21 +1,21 @@
-BEGIN;
-    -- Start a transaction
-    SET @rows_affected = 0;
+SET @rows_affected = 0;
 
-    -- Attempt to delete the user with a specific EmployeeID
-    DELETE FROM employees
-    -- WHERE employeeid = 10;
+START TRANSACTION; -- Start a transaction
 
-    -- Capture the number of rows affected by the DELETE statement
-    SET rows_affected = ROW_COUNT();
+-- Attempt to delete the user with a specific EmployeeID
+DELETE FROM employees
+-- WHERE employeeid = 10;
 
-    -- Conditional logic
-    IF rows_affected = 1 THEN
-        -- Commit the transaction if only one row was deleted
-        COMMIT;
-        SELECT 'Transaction committed: 1 row deleted.' AS Message;
-    ELSE
-        -- Rollback the transaction if more than one or no rows were deleted
-        ROLLBACK;
-        SELECT 'Transaction rolled back: 0 or more than 1 row affected.' AS Message;
-    END IF;
+-- Capture the number of rows affected by the DELETE statement
+SET @rows_affected = ROW_COUNT();
+
+-- Conditional logic
+IF @rows_affected = 1 THEN
+    -- Commit the transaction if only one row was deleted
+    COMMIT;
+    SELECT 'Transaction committed: 1 row deleted.' AS Message;
+ELSE
+    -- Rollback the transaction if no rows or multiple rows were affected
+    ROLLBACK;
+    SELECT 'Transaction rolled back: 0 or more than 1 row affected.' AS Message;
+END IF;
