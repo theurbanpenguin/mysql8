7835857d989606124d2ed980bd6302abfda1c8f6
TheUrbanPenguin <andrew.mallett@theurbanpenguin.com>
2025-01-06 12:15:39 +0000

    add cte example

---
+++ mysql/cte_example.sql
@@ -0,0 +1,34 @@
+WITH RECURSIVE EmployeeHierarchy AS (
+    -- Anchor member: Select top-level employees (those without a manager)
+    SELECT
+        EmployeeID,
+        FirstName AS EmployeeFirstName,
+        LastName AS EmployeeLastName,
+        ReportsTo,
+        CAST(NULL AS CHAR(50)) AS ManagerFirstName,
+        CAST(NULL AS CHAR(50)) AS ManagerLastName
+    FROM Employees
+    WHERE ReportsTo IS NULL
+
+    UNION ALL
+
+    -- Recursive member: Join employees with their managers
+    SELECT
+        e.EmployeeID,
+        e.FirstName AS EmployeeFirstName,
+        e.LastName AS EmployeeLastName,
+        e.ReportsTo,
+        m.EmployeeFirstName AS ManagerFirstName,
+        m.EmployeeLastName AS ManagerLastName
+    FROM Employees e
+    INNER JOIN EmployeeHierarchy m
+        ON e.ReportsTo = m.EmployeeID
+)
+SELECT
+    EmployeeID,
+    EmployeeFirstName,
+    EmployeeLastName,
+    ManagerFirstName,
+    ManagerLastName
+FROM EmployeeHierarchy
+ORDER BY ManagerLastName, EmployeeLastName;
